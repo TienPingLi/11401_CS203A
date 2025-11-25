44c8db55e1a03abf7adffec67e93bff428afaab3
Ping-Li Tien <billyten0208@gmail.com>
2025-11-25 15:50:57 +0800

    Update Makefile.bat

--- Assignment/AssignmentIV/Makefile.bat
+++ Assignment/AssignmentIV/Makefile.bat
@@ -6,8 +6,10 @@ set CC=gcc.exe
 set CXX=g++.exe
 
 REM Flags
-set CFLAGS=-std=c23 -Wall -Wextra -Wpedantic -g
-set CXXFLAGS=-std=c++23 -Wall -Wextra -Wpedantic -g
+REM CFLAGS=-std=c23 -Wall -Wextra -Wpedantic -g
+REM CXXFLAGS=-std=c++23 -Wall -Wextra -Wpedantic -g
+set CFLAGS=-Wall -Wextra -Wpedantic -g
+set CXXFLAGS=-Wall -Wextra -Wpedantic -g
 
 REM Source files
 set C_SRCS=C\main.c C\hash_fn.c
